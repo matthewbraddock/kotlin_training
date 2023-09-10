a184de42ec144ac6305b7cc8aae1a75f6fab7032
matthewbraddock <80083722+matthewbraddock@users.noreply.github.com>
2023-09-10 15:00:34 -0500

    Added website functionality.

--- src/main/kotlin/com/example/whois/controller/WhoisController.kt
+++ src/main/kotlin/com/example/whois/controller/WhoisController.kt
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.http.ResponseEntity
 
 @RestController
 @RequestMapping("api")
 class WhoisController(private val whoIsService: WhoIsService) {
 
     @GetMapping("/getWhoIs")
-    fun getWhoIs(@RequestParam domainName: String): ResponseEntity<String> {
-        val whoIsInfo = whoIsService.getWhoIs(domainName)
-        return ResponseEntity.ok(whoIsInfo)
+    fun getWhoIs(@RequestParam(required = false) domainName: String?): String {
+        if (domainName != null) {
+            val whoIsInfo = whoIsService.getWhoIs(domainName)
+            return whoIsInfo
+        }
+        return "No domainName provided."
     }
 }
+
+
